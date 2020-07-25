8a5cfa8b62b9232e85f3ec23d1f9448a63ff0d94
kshitij <anekix001@gmail.com>
2020-07-25 19:40:10 +0530

    simple load balance request between foked processes

--- koi.py
+++ koi.py
@@ -1,4 +1,5 @@
 import socket
+import array
 
 port = 9090
 host = "127.0.0.1"
@@ -9,63 +10,80 @@ import sys
 
 
 def koi(application, conn):
+	print(f"accepting from {os.getpid()}")
+	environ = {}
+	headers_set = []
+	headers_sent = []
 
-    environ = {}
-    headers_set = []
-    headers_sent = []
+	def write(data):
 
-    def write(data):
+		if not headers_set:
+			raise AssertionError("write() before start_response()")
 
-        if not headers_set:
-            raise AssertionError("write() before start_response()")
+		elif not headers_sent:
+			status, response_headers = headers_sent[:] = headers_set
+			sys.stdout.write("Status: %s\r\n" % status)
+			for header in response_headers:
+				sys.stdout.write("%s: %s\r\n" % header)
+			sys.stdout.write("\r\n")
 
-        elif not headers_sent:
-            status, response_headers = headers_sent[:] = headers_set
-            sys.stdout.write("Status: %s\r\n" % status)
-            for header in response_headers:
-                sys.stdout.write("%s: %s\r\n" % header)
-            sys.stdout.write("\r\n")
+		conn.sendall(data.encode("utf8"))
 
-        conn.sendall(data.encode("utf8"))
+	def start_response(status, response_headers, exec_info=None):
+		if exec_info:
+			try:
+				if headers_sent:
+					raise (exec_info[0], exec_info[1], exec_info[2])
+			finally:
+				exec_info = None
+		elif headers_set:
+			raise AssertionError("Headers already set.")
 
-    def start_response(status, response_headers, exec_info=None):
-        if exec_info:
-            try:
-                if headers_sent:
-                    raise (exec_info[0], exec_info[1], exec_info[2])
-            finally:
-                exec_info = None
-        elif headers_set:
-            raise AssertionError("Headers already set.")
+		headers_set[:] = [status, response_headers]
+		return write
 
-        headers_set[:] = [status, response_headers]
-        return write
+	result = application(environ, start_response)
+	print(result)
 
-    result = application(environ, start_response)
-    print(result)
-
-    try:
-        for data in result:
-            if data:
-                write(data)
-        if not headers_sent:
-            write("")
-    finally:
-        if hasattr(result, "close"):
-            result.close()
-
+	try:
+		for data in result:
+			if data:
+				write(data)
+		if not headers_sent:
+			write("")
+	finally:
+		print('closing connection')
+		conn.close()
+		if hasattr(result, "close"):
+			result.close()
 
 if __name__ == "__main__":
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen(1)
-        print(f"starting koi server on {host}:{port}")
-        conn, addr = s.accept()
-        with conn:
-            print(f"connected by addr {addr}")
-            koi(application, conn)
-            while True:
-                data = conn.recv(1024)
-                # if not data:break
-                # conn.sendall(data)
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((host, port))
+		s.listen(1)
+
+		print(f"starting koi server on {host}:{port}")
+
+		workers = 2
+		for i in range(workers):
+			try:
+				pid = os.fork()
+			except OSError:
+				sys.stderr.write('could not create workers')
+
+			if pid == 0:
+				print(f'Started worker with pid: {os.getpid()}')
+
+
+		while True:
+			conn, addr = s.accept()
+			flo = conn.makefile()
+
+			with conn:
+				koi(application, conn)
+				while True:
+					data = conn.recv(1024)
+
+					if not data:break
+					# conn.sendall(data)
